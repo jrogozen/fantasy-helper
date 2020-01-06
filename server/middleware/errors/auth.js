fa0e37b2ca959c5e76794d488d068b5f785a95fe
Jon Rogozen <jrogozen@hotpads.com>
2020-01-06 11:48:06 -0800

    fix: url redirects on gcloud

--- server/middleware/errors/auth.js
+++ server/middleware/errors/auth.js
@@ -7,11 +7,20 @@ function handleAuthError(error, req, res, next) {
   if (error.code === ERROR_CODES.yahooMissingTokenError) {
     logger.warn('redirect request to yahoo signin');
 
+    if (process.env.GCLOUD_PROJECT) {
+      return res.redirect(301, '/widgets/api/v1/auth/yahoo/signin');
+    }
+
     return res.redirect(301, '/api/v1/auth/yahoo/signin');
   }
 
   if (error.code === ERROR_CODES.yahooInvalidTokenError) {
     logger.warn('redirect request to yahoo refresh');
+
+    if (process.env.GCLOUD_PROJECT) {
+      return res.redirect(301, `/widgets/api/v1/auth/yahoo/refresh?redirect=/widgets/${req.originalUrl}`);
+    }
+
     return res.redirect(301, `/api/v1/auth/yahoo/refresh?redirect=${req.originalUrl}`);
   }
 
